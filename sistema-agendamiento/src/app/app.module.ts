1a50d4e0cf30210680c213e399c51dd3111e4224
BastianCruz <bastian.cruz.mamani@alumnos.uta.cl>
2026-09-12 19:16:23 -0300

    creaciòn de proyecto Angular

---
+++ sistema-agendamiento/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListaSalasComponent } from './components/lista-salas/lista-salas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListaSalasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
